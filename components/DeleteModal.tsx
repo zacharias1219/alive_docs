07f5f7724182d50cbb723c253f8c8e35d6f27e5a
Richard Abishai <rickoshade1891@gmail.com>
2024-08-07 20:26:48 +0530

    feat: Add @sentry/nextjs dependency and update package.json
    This commit adds the "@sentry/nextjs" dependency to the project, specifically version 8.24.0. It also updates the package.json file to include this new dependency. This change enables the integration of Sentry error tracking in the Next.js application, improving error monitoring and debugging capabilities.

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -1,36 +1,83 @@
-import { cn } from '@/lib/utils';
-import { useIsThreadActive } from '@liveblocks/react-lexical';
-import { Composer, Thread } from '@liveblocks/react-ui';
-import { useThreads } from '@liveblocks/react/suspense';
-import React from 'react'
+"use client";
 
-const ThreadWrapper = ({ thread }: ThreadWrapperProps) => {
-  const isActive = useIsThreadActive(thread.id);
+import Image from "next/image";
+import { useState } from "react";
+
+import { deleteDocument } from "@/lib/actions/room.actions";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Button } from "./ui/button";
+
+export const DeleteModal = ({ roomId }: DeleteModalProps) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const deleteDocumentHandler = async () => {
+    setLoading(true);
+
+    try {
+      await deleteDocument(roomId);
+      setOpen(false);
+    } catch (error) {
+      console.log("Error notif:", error);
+    }
+
+    setLoading(false);
+  };
 
   return (
-    <Thread 
-      thread={thread}
-      data-state={isActive ? 'active' : null}
-      className={cn('comment-thread border', 
-        isActive && '!border-blue-500 shadow-md',
-        thread.resolved && 'opacity-40'
-      )}
-    />
-  )
-}
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="min-w-9 rounded-xl bg-transparent p-2 transition-all">
+          <Image
+            src="/assets/icons/delete.svg"
+            alt="delete"
+            width={20}
+            height={20}
+            className="mt-1"
+          />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog">
+        <DialogHeader>
+          <Image
+            src="/assets/icons/delete-modal.svg"
+            alt="delete"
+            width={48}
+            height={48}
+            className="mb-4"
+          />
+          <DialogTitle>Delete document</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete this document? This action cannot be
+            undone.
+          </DialogDescription>
+        </DialogHeader>
 
-const Comments = () => {
-  const { threads } = useThreads();
+        <DialogFooter className="mt-5">
+          <DialogClose asChild className="w-full bg-dark-400 text-white">
+            Cancel
+          </DialogClose>
 
-  return (
-    <div className="comments-container">
-      <Composer className="comment-composer" />
-
-      {threads.map((thread) => (
-        <ThreadWrapper key={thread.id} thread={thread} />
-      ))}
-    </div>
-  )
-}
-
-export default Comments
+          <Button
+            variant="destructive"
+            onClick={deleteDocumentHandler}
+            className="gradient-red w-full"
+          >
+            {loading ? "Deleting..." : "Delete"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
